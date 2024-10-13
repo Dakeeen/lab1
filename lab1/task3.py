7cd871054d3dff43e26968bc02abcfb2d98dfab0
Dakeeen <vsf5199@gmail.com>
2024-10-13 15:50:11 +0300

    Add files via upload

---
+++ lab1/task3.py
@@ -0,0 +1,9 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+middle = len(list_players)// 2  #3
+amount = len(list_players)
+teamone = list_players[:middle]
+teamtwo = list_players[middle:]
+print(teamone)
+print(teamtwo)
+
+
